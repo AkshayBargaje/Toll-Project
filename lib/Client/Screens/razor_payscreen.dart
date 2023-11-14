95f41e6ca48d78de10998282eb285e88bbd60fc0
Akshay <81340398+AkshayBargaje@users.noreply.github.com>
2023-11-14 19:21:07 +0530

    Add existing project files to Git

---
+++ lib/Client/Screens/razor_payscreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RazorPayScreen extends StatelessWidget {
+  const RazorPayScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Razor page page"),
+    );
+  }
+}
